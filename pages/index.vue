190c23da58183a1fe83e676cab56b47b5ccd4aa5
compro-rohitkumar <rohit.kumar@comprotechnologies.com>
2023-09-13 17:20:14 +0530

    QuizQuestion Feature added

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div>
       <h1 class="text-2xl font-semibold text-center pt-10">Quiz</h1>
   
-      <QuizQuestion v-for="(question, index) in question" :key="index" :question="question" :indexs="index" :options="options[index]"  :questionIndex="index" @selectAnswer="changeAnswer"/>
+      <QuizQuestion v-for="(question, index) in question" :key="index" :question="question"  :options="options[index]"  :questionIndex="index" @selectAnswer="changeAnswer"/>
       <div class="flex justify-center my-10">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " @click="checkAnswers">Check Answers</button>
       </div>
@@ -10,9 +10,11 @@
   </template>
   
   <script setup>
+
+
     const question = ref(["What is the capital of France?","What is the capital of England?","What is the capital of Germany?","What is the capital of Spain?"]);
     const options = ref([["Paris", "London", "Berlin", "Madrid"],["Paris", "London", "Berlin", "Madrid"],["Paris", "London", "Berlin", "Madrid"],["Paris", "London", "Berlin", "Madrid"]]);
-    const answer = ref(["Paris","London","Berlin","Madrid"]);
+    const answer = ref(["paris","London","Berlin","Madrid"]);
     const defaultAnswer = ref(["","","",""]);
     const changeAnswer = (payload) => {
         const {option,index} = payload;
