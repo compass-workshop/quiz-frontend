19e546b35e41acf9dd459c106dc9b6e40605a698
Gopal Gupta <gopal.gupta@comprotechnologies.com>
2023-09-15 17:48:01 +0530

    Implement dashboard page

---
+++ components/Analytics.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="grid grid-cols-2 text-orange-500 text-2xl gap-x-64 border-b-4 border-orange-500 w-full mt-10 mb-10 gap-y-4 p-6">
+        <div>Test Attempted: {{ data.attempted }}</div>
+        <div>Passed Test: {{ data.passed }}</div>
+        <div>Average Score: {{ data.average_score }}</div>
+        <div>Failed Test: {{ data.failed }}</div>
+        <div>Best Score: {{ data.best_score }}</div>
+    </div>
+</template>
+
+<script setup>
+  const data = { // will fetch from api
+    attempted: 5,
+    average_score: 15,
+    best_score: 18,
+    passed: 3,
+    failed: 2
+  }
+
+</script>
+
+<style scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <!-- make these 3 lines a component "Header" -->
+        <header class="text-3xl font-bold dark:text-white p-4 text-red-500 drop-shadow-lg border-b-4 border-black">
+            <h2>QUIZ APP</h2>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
