d1ea3fe1abad092a7d3a5919ca0f72bd5d9fe9f5
Gopal Gupta <gopal.gupta@comprotechnologies.com>
2023-09-16 17:28:08 +0530

    Add fetch api for tests

--- components/Testslist.vue
+++ components/Testslist.vue
@@ -3,11 +3,11 @@
         <div v-for="test in data" :key="test.id">
             <div class="grid grid-cols-2 gap-x-64 w-full mt-10 mb-10 pl-6 text-2xl">
                 <div>
-                    {{ test.id }} | {{ test.name }}
+                    {{ test.name }}
                 </div>
                 <div class="">
                     <span>
-                        <button>Launch</button>
+                        <NuxtLink to="/user/:userId/test/:testId/launch">Launch</NuxtLink>
                     </span>
                     <span :class="{'invisible': test.is_review}">
                         <button>&nbsp;| Review</button>
@@ -19,29 +19,7 @@
 </template>
 
 <script setup>
-  const data =
-    [
-        {
-            id: 123,
-            name: "Test1",
-            is_review: false
-        },
-        {
-            id: 456,
-            name: "Test2",
-            is_review: true
-        },
-        {
-            id: 789,
-            name: "Test3",
-            is_review: false
-        },
-        {
-            id: 102,
-            name: "Test4",
-            is_review: true
-        }
-    ]
+    const { data }=await useFetch('http://localhost:3000/tests');
 </script>
 
 <style scoped>
