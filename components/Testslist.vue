3bbbc2abf98fc539696559155352a3ac991db1bc
Gopal Gupta <gopal.gupta@comprotechnologies.com>
2023-09-18 12:47:46 +0530

    Add button css and analytics api

--- components/Testslist.vue
+++ components/Testslist.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
-        <div v-for="test in data" :key="test.id"
-        class="shadow-lg">
+        <div v-for="test in data" :key="test.id" class="shadow-lg">
             <div class="grid grid-cols-2 gap-x-64 w-full mt-10 mb-10 pl-6 py-4 text-2xl hover:shadow-xl">
                 <div>
                     {{ test.name }}
                 </div>
                 <div>
                     <span>
-                        <NuxtLink :to="`/test/${test.id}/launch`">Launch</NuxtLink>
+                        <button class="border-4 border-red-800 p-2 bg-yellow-300 rounded-full">
+                            <NuxtLink :to="`/test/${test.id}/launch`">Launch</NuxtLink>
+                        </button>
                     </span>
                     <span :class="{'invisible': test.is_review}">
-                        <button>&nbsp;| Review</button>
+                        <span>&nbsp;| </span>
+                        <button class="border-4 border-red-800 p-2 bg-yellow-300 rounded-full">Review</button>
                     </span>
                 </div>
             </div>
