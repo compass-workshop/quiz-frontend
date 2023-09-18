70cd19874d14f6345a30bc177a260ee2618b55e4
Gopal Gupta <gopal.gupta@comprotechnologies.com>
2023-09-18 12:47:46 +0530

    Add button css and analytics api

--- components/Analytics.vue
+++ components/Analytics.vue
@@ -1,22 +1,15 @@
 <template>
     <div class="grid grid-cols-2 text-orange-500 text-2xl gap-x-64 border-b-4 border-orange-500 w-full mt-10 mb-10 gap-y-4 p-6">
-        <div>Test Attempted: {{ data.attempted }}</div>
-        <div>Passed Test: {{ data.passed }}</div>
-        <div>Average Score: {{ data.average_score }}</div>
-        <div>Failed Test: {{ data.failed }}</div>
-        <div>Best Score: {{ data.best_score }}</div>
+        <div>Test Attempted: {{ data[0].TOTAL_TEST }}</div>
+        <div>Passed Test: {{ data[0].PASSED_TEST }}</div>
+        <div>Average Score: {{ data[0].AVG_SCORE }}</div>
+        <div>Failed Test: {{ data[0].TOTAL_TEST - data[0].PASSED_TEST }}</div>
+        <div>Best Score: {{ data[0].MAX_SCORE }}</div>
     </div>
 </template>
 
 <script setup>
-  const data = { // will fetch from api
-    attempted: 5,
-    average_score: 15,
-    best_score: 18,
-    passed: 3,
-    failed: 2
-  }
-
+  const {data} = await useFetch('http://localhost:3000/analytics/1');
 </script>
 
 <style scoped>
--- components/Testslist.vue
+++ components/Testslist.vue
@@ -2,15 +2,18 @@
     <div>
         <div v-for="test in data" :key="test.id">
             <div class="grid grid-cols-2 gap-x-64 w-full mt-10 mb-10 pl-6 text-2xl">
-                <div>
+                <div class="text-lime-700 text-3xl"> 
                     {{ test.name }}
                 </div>
                 <div>
                     <span>
-                        <NuxtLink :to="`/test/${test.id}/launch`">Launch</NuxtLink>
+                        <button class="border-4 border-red-800 p-2 bg-yellow-300 rounded-full">
+                            <NuxtLink :to="`/test/${test.id}/launch`">Launch</NuxtLink>
+                        </button>
                     </span>
                     <span :class="{'invisible': test.is_review}">
-                        <button>&nbsp;| Review</button>
+                        <span>&nbsp;| </span>
+                        <button class="border-4 border-red-800 p-2 bg-yellow-300 rounded-full">Review</button>
                     </span>
                 </div>
             </div>
